fe2b72c0354ce671fc7ac84de278141b18edb1a3
Ale-1407 <alessiacarrera07@gmail.com>
2022-10-18 15:54:17 +0200

    richiesta cognome all'utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,8 @@
 
 let userName = prompt('Qual è il tuo nome?');
 console.log(userName);
+
+/* chiedere all'utente il suo nome */
+
+let userSurname = prompt('Qual è il tuo cognome?');
+console.log(userSurname);
